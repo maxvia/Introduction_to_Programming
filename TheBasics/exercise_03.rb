68389ca683716ad9b61abfb8a7d2b36055bebaae
Max Diak <max@via-prima.com>
2015-06-03 15:45:23 +0400

    The Basics - finished

---
+++ TheBasics/exercise_03.rb
@@ -0,0 +1,9 @@
+hash = { 
+  "Apocalipsis Now" => 1975, 
+  "Usual Suspects" => 1996, 
+  "American Beauty" => 1995, 
+  "The Game" => 1998, 
+  "Hunted" => 2002}
+puts hash["Apocalipsis Now"]
+puts hash["Usual Suspects"]
+puts hash["American Beauty"]
---
+++ TheBasics/exercise_04.rb
@@ -0,0 +1,17 @@
+hash = { 
+  "Apocalipsis Now" => 1975,
+  "Usual Suspects" => 1996,
+  "American Beauty" => 1995,
+  "The Game" => 1998,
+  "Hunted" => 2002}
+array = [
+  hash["Apocalipsis Now"],
+  hash["Usual Suspects"],
+  hash["American Beauty"],
+  hash["The Game"],
+  hash["Hunted"] ]
+puts array[0]
+puts array[1]
+puts array[2]
+puts array[3]
+puts array[4]
